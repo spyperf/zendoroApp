f84698f8cf7185670c068836621287aed7fea5c3
spyperf <spyperf@gmail.com>
2026-02-11 07:54:34 -0500

    Audio + Saving Progress
    Even though the code for the audio is very basic, I will try to implement a volume control and a dropdown menu where you can select audio in my next iteration. Additionally, I will develop buttons that will direct you back to the last panel, as well as code the "Reset Timer" button to work.
    
    Last two steps: Implementing a leaderboard system if possible, and creating custom designs/aesthetics.

--- zendoro/src/TimerPanel.java
+++ zendoro/src/TimerPanel.java
@@ -1,5 +1,8 @@
 
 import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+import javax.sound.sampled.*;
 import javax.swing.*;
 
 public class TimerPanel extends JPanel {
@@ -11,6 +14,7 @@ public class TimerPanel extends JPanel {
     private int studyTime;
     private int originalStudyTime;
     private int studyPeriods;
+    private int originalStudyPeriods;
     private boolean isStudying = true;
 
     public TimerPanel(int studyPeriods, int studyTime, int breakTime) {
@@ -19,6 +23,8 @@ public class TimerPanel extends JPanel {
         originalBreakTime = this.breakTime;
         originalStudyTime = this.studyTime;
         this.studyPeriods = studyPeriods;
+        this.originalStudyPeriods = studyPeriods;
+        int previousMinutes = zendoro.savedMinutes;
         setBackground(Color.PINK);
         setLayout(null);
         setBounds(0, 0, 480, 720);
@@ -42,13 +48,35 @@ public class TimerPanel extends JPanel {
         timerLabel.setFont(new Font("Arial", Font.BOLD, 40));
         timerLabel.setBounds(80, 300, 320, 80);
         timerLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        // White Noise Audio
+        try {
+            File whiteNoiseFile = new File("C:\\Users\\spype\\OneDrive\\Documents\\ICS4U (AP CS)\\zendoro\\white_noise.wav");
+            AudioInputStream audioStreamWhite = AudioSystem.getAudioInputStream(whiteNoiseFile);
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioStreamWhite);
+            clip.start();
+            clip.loop(Clip.LOOP_CONTINUOUSLY);
+        } catch (Exception exc) {
+        }
+        /*
+        // Clock ticking
+        try {
+            File clockTickingFile = new File("C:\\Users\\spype\\OneDrive\\Documents\\ICS4U (AP CS)\\zendoro\\clock_ticking.wav");
+            AudioInputStream audioStreamClock = AudioSystem.getAudioInputStream(clockTickingFile);
+            Clip clip = AudioSystem.getClip();
+            clip.open(audioStreamClock);
+            clip.start();
+            clip.loop(Clip.LOOP_CONTINUOUSLY);
+        } catch (Exception exc) {
+        }
+        */
         add(timerLabel);
         add(amountOfStudyPeriods);
         setBackground(new Color(255, 150, 150));
         add(studyingLabel);
-        timer = new Timer(10, e -> {
+        timer = new Timer(1000, e -> {
             // Is currently studying
-            if (isStudying == true) {
+            if (isStudying == true && this.studyPeriods > 0) {
                 add(studyingLabel);
                 remove(breakingLabel);
                 setBackground(new Color(255, 150, 150));
@@ -57,10 +85,11 @@ public class TimerPanel extends JPanel {
                 this.studyTime--;
                 // minutes : seconds
                 timerLabel.setText(String.valueOf(this.studyTime / 60 + ":" + this.studyTime % 60));
+                // When study time has ended
                 if (this.studyTime <= 0) {
                     this.breakTime = originalBreakTime;
                     this.studyPeriods--;
-                    amountOfStudyPeriods.setText("Number of Study Periods: " + String.valueOf(studyPeriods));
+                    amountOfStudyPeriods.setText("Number of Study Periods: " + String.valueOf(this.studyPeriods));
                     isStudying = false;
                 } else if (this.studyTime % 60 < 10) {
                     timerLabel.setText(this.studyTime / 60 + ":" + "0" + this.studyTime % 60);
@@ -74,7 +103,8 @@ public class TimerPanel extends JPanel {
                 repaint();
                 this.breakTime--;
                 timerLabel.setText(String.valueOf(this.breakTime / 60 + ":" + this.breakTime % 60));
-                if (breakTime <= 0) {
+                // When break time has ended
+                if (this.breakTime <= 0) {
                     this.studyTime = originalStudyTime;
                     isStudying = true;
                 } else if (this.breakTime % 60 < 10) {
@@ -83,14 +113,45 @@ public class TimerPanel extends JPanel {
             }
             if (this.studyPeriods <= 0) {
                 timer.stop();
-                timerLabel.setText("0:00");
-                return;
+                // Total minutes studied is saved
+                int totalMinutes = originalStudyTime * originalStudyPeriods / 60;
+                try {
+                    BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt"));
+                    writer.write(previousMinutes + totalMinutes + " minutes studied");
+                    writer.close();
+                    // variable e is already defined in constructor TimerPanel(int,int,int), so change to "ex"
+                } catch (IOException ex) {
+                    // TODO Auto-generated catch block
+                    ex.printStackTrace();
+                }
+                setBackground(Color.PINK);
+                timerLabel.setVisible(false);
+                breakingLabel.setVisible(false);
+                studyingLabel.setVisible(false);
+                amountOfStudyPeriods.setVisible(false);
+                instructions.setVisible(false);
+                revalidate();
+                repaint();
             }
         });
         timer.start();
-
         // When reset timer button is hit:
+        JButton resetButton = new JButton("Reset Timer");
+        resetButton.setBounds(100, 600, 300, 40);
+        add(resetButton);
+        resetButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                timer.stop();
+                setBackground(Color.BLACK);
+                timerLabel.setVisible(false);
+                breakingLabel.setVisible(false);
+                studyingLabel.setVisible(false);
+                amountOfStudyPeriods.setVisible(false);
+                instructions.setVisible(false);
+                resetButton.setVisible(false);
+                revalidate();
+                repaint();
+            }
+        });
     }
 }
-
-// FIX BREAK TIME ISSUE IT IS NOT SOTOPPING
--- zendoro/src/zendoro.java
+++ zendoro/src/zendoro.java
@@ -1,13 +1,44 @@
+
+import java.io.*;
 import javax.swing.*;
 
 public class zendoro {
-
+    public static int savedMinutes = 0;
     public static void main(String[] args) {
+        JLabel minutesStudied = new JLabel();
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("output.txt"));
+            String line = reader.readLine();
+            reader.close();
+            // Checks if line read is null or if its empty
+            if (line != null && !line.isEmpty()) {
+                // checks for space
+                line = line.trim();
+                if (line.indexOf(" ") == -1) {
+                    savedMinutes = Integer.parseInt(line);
+                }
+                else {
+                    savedMinutes = Integer.parseInt(line.substring(0,line.indexOf(" ")));
+                }
+            }
+            else {
+                savedMinutes = 0;
+            }
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            // File not exisiting?
+            savedMinutes = 0;
+            e.printStackTrace();
+        }
+        minutesStudied.setText(savedMinutes + " minutes studied");
+        minutesStudied.setBounds(0, 100, 480, 100);
+        minutesStudied.setHorizontalAlignment(SwingConstants.CENTER);
         JFrame frame = new JFrame("zendoro");
         frame.setSize(480, 720);
         frame.setLayout(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
+        frame.add(minutesStudied);
         TitlePanel titleScreen = new TitlePanel();
         frame.add(titleScreen);
         frame.setVisible(true);
